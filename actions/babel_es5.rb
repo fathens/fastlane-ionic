43d6d2dae9c8b8885886340821c891d2d17fd47f
Kunio SAWATANI <fsawa@fathens.org>
2017-01-27 01:30:21 +0000

    babel_es5 に presets オプションを追加

--- actions/babel_es5.rb
+++ actions/babel_es5.rb
@@ -2,15 +2,19 @@ module Fastlane
   module Actions
     class BabelEs5Action < Action
       def self.run(params)
-        babel(Pathname('www')/'build'/'main.js', params[:compress])
+        presets = params[:presets]
+        presets = presets.split(',').map { |x| x.strip } if (presets.is_a? String)
+        babel(Pathname('www')/'build'/'main.js', presets, params[:compress])
         add_runtime(Pathname('node_modules')/'regenerator-runtime'/'runtime.js')
       end
 
-      def self.babel(src, compress)
-        UI.message "babel with#{compress ? '' : 'out'} compress"
+      def self.babel(src, presets, compress)
+        UI.message "babel #{presets} with#{compress ? '' : 'out'} compress"
         tmp = src.dirname/'es5.js'
-        sh('npm install babel-cli babel-preset-es2015 babel-preset-stage-0')
-        sh("babel --presets=es2015,stage-0 -o #{tmp} #{src}")
+
+        sh("npm install babel-cli #{presets.map { |x| "babel-preset-#{x}" }.join(' ') }")
+        sh("babel --presets=#{presets.join(',')} -o #{tmp} #{src}")
+
         if compress then
           sh('npm install uglify-js')
           sh("uglifyjs --compress --mangle --output #{src} -- #{tmp}")
@@ -54,6 +58,13 @@ module Fastlane
 
       def self.available_options
         [
+          FastlaneCore::ConfigItem.new(key: :presets,
+          env_name: 'BABEL_PRESETS',
+          description: "Presets for using by babel",
+          optional: true,
+          default_value: ['es2015', 'stage-0'],
+          is_string: false
+          ),
           FastlaneCore::ConfigItem.new(key: :compress,
           description: "Compress after babel by uglifyjs",
           optional: true,
